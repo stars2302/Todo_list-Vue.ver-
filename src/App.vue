ab982206938559f27233996c8285be3dadd1871b
stars2302 <stars2302@naver.com>
2023-10-26 14:39:35 +0900

    [FEAT] todo_list 기본기능구현완료

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script>
+  import Todo from './components/Todo.vue';
+
   export default{
+    components:{
+      Todo
+    },
     data(){
       return{
         todoText: '',//text입력시 바뀔 값
@@ -8,6 +13,27 @@
           {id: 2, text:'스밍돌리기', checked: false}
         ]
 
+      }
+    },
+    methods:{
+      AddTodo(e){
+        // console.log(e.target.value);
+        this.todos.push({
+          id: Math.random(), 
+          text: e.target.value, 
+          checked: false
+        });
+        this.todoText ='';
+      },
+      toggleCheck({id,checked}){
+        const idx = this.todos.findIndex(todo => todo.id === id); //조건에 부합하는 아이의 index를 추출
+        this.todos[idx].checked = checked;
+      },
+      deleteTodo(id){
+        if(confirm('정말 삭제하시겠습니까?')){
+          const idx = this.todos.findIndex(todo => todo.id === id);
+          this.todos.splice(idx,1);
+        }
       }
     }
   }
@@ -19,11 +45,19 @@
 
     <div class="mb-3">
       <label for="todo" class="form-label">Todo Input</label>
-      <input type="text" class="form-control" id="todo" placeholder="오늘의 할 일을 작성해주세요!">
+      <input 
+        type="text" 
+        class="form-control" 
+        id="todo" 
+        placeholder="오늘의 할 일을 작성해주세요!"
+        @keyup.enter="AddTodo"
+        v-model="todoText"
+      >
     </div>
     <hr>
-    <Todo v-for="todo in todos" :key="todo.id" :text="todo.text"/>
-  </div>
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" @toggle-checkbox="toggleCheck" @click-delete="deleteTodo"/>
+    
+  </div><!--container-->
 </template>
 
 <style scoped>
---
+++ src/components/Todo.vue
@@ -0,0 +1,39 @@
+<script>
+  export default {
+    props:{
+      todo:{
+        type:Object, //데이터 타입
+        required:true //필수여부
+      }
+    },
+    methods:{
+      toggleCheckbox(e){
+        this.$emit('toggle-checkbox',{
+          id: this.todo.id,
+          checked: e.target.checked
+        })
+      },
+      clickDel(){
+        //this.$emit('부모에게 받은 함수',함수에 넘길 파라미터)
+        this.$emit('click-delete',this.todo.id);
+      }
+    }
+
+  }
+</script>
+
+<template>
+  <div class="form-check" :data-id="todo.id">
+    <input 
+      class="form-check-input" 
+      type="checkbox" 
+      value="" 
+      :id="'input'+todo.id" 
+      @change="toggleCheckbox"
+    >
+    <label class="form-check-label" :for="`input${todo.id}`" :style="todo.checked ? 'text-decoration: line-through; opacity:0.5':''">
+      {{todo.text}}
+    </label>
+    <button type="button" class="btn btn-danger btn-sm ms-2" @click="clickDel">Delete</button>
+  </div>
+</template>
